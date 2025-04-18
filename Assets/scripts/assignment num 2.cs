d6f0a2d24bf6084beb65476d3471a601d98524c8
raed-abodayy <raedabodyu@gmail.com>
2025-04-18 17:28:32 +0300

    little changes in the code

--- Assets/scripts/assignment num 2.cs
+++ Assets/scripts/assignment num 2.cs
@@ -1,21 +1,19 @@
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class assignmentnum2 : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
-   {
-string[] shop = new string[10]{"Cat","Dog","car","pizza","hat","fish","tree","monkey","ball","bird"};
-int counter=0;
-while(counter<7)
-{Debug.Log(shop[Random.Range(1,10)]);
-counter++;
-}
+    {
+        string[] shop = { "Cat", "Dog", "car", "pizza", "hat", "fish", "tree", "monkey", "ball", "bird" };
+        string result = "";
+        int counter = 0;
 
+        while (counter < 7)
+        {
+            result += shop[Random.Range(0, 10)] + " ";
+            counter++;
+        }
 
-
+        Debug.Log(result);
     }
 }
